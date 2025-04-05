c8745e8405d4fe559384bf4d69ed2aea3e6beece
David Walker <david.walker5@icloud.com>
2025-04-05 18:56:57 +0100

    Update the version number

--- src/chess_analyser/constants/__init__.py
+++ src/chess_analyser/constants/__init__.py
@@ -1,7 +1,7 @@
 # Version Information
 PROGRAM_NAME = "Chess Analyser"
 PROGRAM_DESCRIPTION = "Game analysis using UCI engines"
-PROGRAM_VERSION = "1.7.2"
+PROGRAM_VERSION = "1.8.0"
 
 # Members of the analysis options dictionary
 OPT_LOAD = "load"
