bb20c52a091728717c5751cdd8859ae24bfecc72
Dave Walker <david.walker5@icloud.com>
2026-01-22 15:00:46 +0000

    Finalise wording in README and naming in Jupyter notebooks

--- performance-modelling/update.sh
+++ performance-modelling/update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -f
 
-ELO_MODELLING_ROOT=$( cd "$( dirname "$0" )" && pwd )
-. $ELO_MODELLING_ROOT/venv/bin/activate
+PERFORMANCE_MODELLING_ROOT=$( cd "$( dirname "$0" )" && pwd )
+. $PERFORMANCE_MODELLING_ROOT/venv/bin/activate
 
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U 
 pip freeze > requirements.txt
