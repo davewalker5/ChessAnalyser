321b30c0937c351fca2be40ced263aba6a6ec13b
Dave Walker <david.walker5@icloud.com>
2026-06-23 11:16:23 +0100

    Convert main app to pyproject.toml

--- make_venv.sh
+++ make_venv.sh
@@ -1,5 +1,15 @@
 #!/bin/bash -f
 
+# If CLANG is installed via MacPorts, use it and the gfortran compiler as
+# pre-requisites for building SciPy
+if command -v port >/dev/null 2>&1 && port -q installed clang-17 >/dev/null 2>&1; then
+    echo "Setting up clang from MacPorts"
+    export CC=/opt/local/bin/clang-mp-17
+    export CXX=/opt/local/bin/clang++-mp-17
+    export FC=/opt/local/bin/gfortran-mp-13
+    export PKG_CONFIG_PATH=/opt/local/lib/pkgconfig
+fi
+
 # Deactivate and remove the old virtual environment, if present
 echo "Removing existing Virtual Environment, if present ..."
 deactivate 2> /dev/null || true
@@ -13,5 +23,5 @@ python -m venv venv
 # Make sure pip is up to date
 pip install --upgrade pip
 
-# Install the requirements
-pip install -r requirements.txt
+# Install the analyser and its direct dependencies
+pip install --editable .
