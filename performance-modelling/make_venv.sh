bded7ca77cedc15274740edb9c4a5ef2e2447fe2
Dave Walker <david.walker5@icloud.com>
2026-06-23 11:07:33 +0100

    Convert performance modelling to pyproject.toml

--- performance-modelling/make_venv.sh
+++ performance-modelling/make_venv.sh
@@ -1,17 +1,19 @@
 #!/bin/bash -f
 
+PERFORMANCE_MODELLING_ROOT=$( cd "$( dirname "$0" )" && pwd )
+
 # Deactivate and remove the old virtual environment, if present
 echo "Removing existing Virtual Environment, if present ..."
 deactivate 2> /dev/null || true
-rm -fr venv
+rm -fr "$PERFORMANCE_MODELLING_ROOT/venv"
 
 # Create a new environment and activate it
 echo "Creating new Virtual Environment ..."
-python -m venv venv
-. venv/bin/activate
+python -m venv "$PERFORMANCE_MODELLING_ROOT/venv"
+. "$PERFORMANCE_MODELLING_ROOT/venv/bin/activate"
 
-# Make sure pip is up to date
-pip install --upgrade pip
+# Make sure packaging tools are up to date
+pip install --upgrade pip setuptools
 
-# Install the requirements
-pip install -r requirements.txt
+# Install the reporting suite dependencies
+pip install --no-build-isolation -e "$PERFORMANCE_MODELLING_ROOT"
